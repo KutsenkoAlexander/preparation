94bd94c45ff3e5010172db989aaa850add21b6de
Aleksandr.Kutsenko <aleksandr.kutsenko@techincity.ru>
2024-01-06 18:17:07 +0300

    #83. Remove Duplicates from Sorted List

---
+++ src/my/study/coder/path/leetcode/MergeSortedArray.java
@@ -0,0 +1,11 @@
+package my.study.coder.path.leetcode;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
+    }
+
+    private static void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.out.println("Do merge");
+    }
+}
