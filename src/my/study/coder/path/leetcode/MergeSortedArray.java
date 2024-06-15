895a768379c74f8e9645b901c3762eca23a070ef
Александр Куценко <kucenkoaleksandr@gmail.com>
2024-06-15 21:14:01 +0300

    88. Merge Sorted Array

--- src/my/study/coder/path/leetcode/MergeSortedArray.java
+++ src/my/study/coder/path/leetcode/MergeSortedArray.java
@@ -1,11 +1,26 @@
 package my.study.coder.path.leetcode;
 
+import java.util.Arrays;
+
 public class MergeSortedArray {
     public static void main(String[] args) {
-        merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
+        System.out.println(Arrays.toString(merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3)));
+        System.out.println(Arrays.toString(merge(new int[]{1}, 1, new int[]{}, 0)));
+        System.out.println(Arrays.toString(merge(new int[0], 0, new int[]{1}, 1)));
     }
 
-    private static void merge(int[] nums1, int m, int[] nums2, int n) {
-        System.out.println("Do merge");
+    private static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        return nums1;
     }
 }
